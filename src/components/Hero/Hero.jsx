410633182bae70783c6ff08133b57a2a746e7662
binyul <jiibinyul@gmail.com>
2024-08-10 23:57:01 +0545

    working_hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,61 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import BgImage from "../../assets/bg-slate.png";
+import BlackCoffee from "../../assets/black.png";
 
 const bgImage = {
   backgroundImage: `url(${BgImage})`,
   backgroundSize: "cover",
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
-  
 };
 
 const Hero = () => {
-  return( 
-  <main style={bgImage}>
-    <section className="min-h-[750px] w-full">
-      
-      <div className="container">
-        {/* Nav section */}
-        {/* Hero section */}
-        <div className="grid grid-cols-1 md:grid-cols-2"></div>
-      </div>
-    </section>
-  </main>
+  return (
+    <main style={bgImage}>
+      <section className="min-h-[750px] w-full">
+        <div className="container">
+          {/* Nav section */}
+          {/* Hero section */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px] ">
+            {/* text content section */}
+            <div className="text-lightOrange mt-[100px] md-mt-0 p-4 space-y-28">
+              <h1 className="text-7xl font-bold leading-tight ml-14">
+                Black thumbler
+              </h1>
+              <div className="relative">
+                <div className="relative z-10 space-y-4">
+                  <h1 className="text-2xl">Black Lifestyle Lover</h1>
+                  <h1 className="text-sm opacity-55">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Veniam blanditiis cumque nemo molestias repudiandae aliquam
+                    laboriosam, modi a omnis perferendis cupiditate voluptatum
+                  </h1>
+                </div>
+                <div className="absolute -top-6 -left-10 w-[250px] h-[180px] bg-gray-700/25"></div>
+              </div>
+            </div>
+            {/* Hero Image section */}
+            <div className="relative ">
+              <img
+                src={BlackCoffee}
+                alt=""
+                className="relative z-40 h-[400px] md:h-[700px]  image-shadow"
+              />
+              {/* orange ring circle */}
+              <div
+                className="h-[180px] w-[180px] absolute top-24 -right-20
+                border-primary border-[20px] rounded-full z-10"
+              ></div>
+              <div></div>
+            </div>
+            {/* third div section */}
+            <div></div>
+          </div>
+          <div className=""></div>
+        </div>
+      </section>
+    </main>
   );
 };
 
