25fe225a0af04049245ba04f2444bab0961b4cf4
binyul <jiibinyul@gmail.com>
2024-08-11 10:24:44 +0545

    motioninstalled

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import BgImage from "../../assets/bg-slate.png";
 import BlackCoffee from "../../assets/black.png";
 import Navbar from "../Navbar/Navbar";
+import { motion } from "framer-motion";
 
 const bgImage = {
   backgroundImage: `url(${BgImage})`,
@@ -22,10 +23,28 @@ const Hero = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px] ">
             {/* text content section */}
             <div className="text-lightOrange mt-[100px] md-mt-0 p-4 space-y-28">
-              <h1 className="text-7xl font-bold leading-tight ml-14">
+              <motion.h1
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1,
+                 }}
+               className="text-7xl font-bold leading-tight ml-14">
                 Black thumbler
-              </h1>
-              <div className="relative">
+              </motion.h1>
+              <motion.div
+               initial={{ opacity: 0, y: 100 }}
+               animate={{ opacity: 1, y: 0 }}
+               transition={{ 
+                 type: "spring",
+                 stiffness: 100,
+                 damping: 10,
+                 delay: 1.2,
+                }}               
+                className="relative">
                 <div className="relative z-10 space-y-4">
                   <h1 className="text-2xl">Black Lifestyle Lover</h1>
                   <h1 className="text-sm opacity-55">
@@ -35,7 +54,7 @@ const Hero = () => {
                   </h1>
                 </div>
                 <div className="absolute -top-6 -left-10 w-[250px] h-[180px] bg-gray-700/25"></div>
-              </div>
+              </motion.div>
             </div>
             {/* Hero Image section */}
             <div className="relative ">
@@ -62,13 +81,22 @@ const Hero = () => {
                 Black thumbler
               </h1>
               <div className="relative">
-                <div className="relative z-10 space-y-4">
+                <motion.div 
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1.2,
+                 }} 
+                className="relative z-10 space-y-4">
                   <h1 className="text-2xl">Black Lifestyle Lover</h1>
                   <h1 className="text-sm opacity-55">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Veniam blanditiis cumque
                   </h1>
-                </div>
+                </motion.div>
                 <div className="absolute -top-6 -right-10 w-[250px] h-[180px] bg-darkGray/50"></div>
               </div>
             </div>
